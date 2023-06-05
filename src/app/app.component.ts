ee79e6ba9785a4801c73293cdbb09ab49b2c3c6e
CristinaJordi <cristina.jordi@bluewin.ch>
2023-06-05 16:20:50 +0200

    To top added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   title = 'portfolio';
+  showButton = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.showButton = window.pageYOffset > 200;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
